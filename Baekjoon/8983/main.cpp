26e938f8b5c52f5053078a3004c88bfbca56a953
san9640 <44027624+san9640@users.noreply.github.com>
2019-08-27 17:10:51 +0900

    사냥꾼

---
+++ Baekjoon/8983/main.cpp
@@ -0,0 +1,23 @@
+#include<ios>
+#include<vector>
+#include<algorithm>
+int m, l, x, y, z;
+std::vector<int> v = { -(int)2e9, (int)2e9 };
+
+int f(int k) {
+	int a = 0, b = m, c;
+	while (a + 1 < b)
+		if (v[c = (a + b) / 2] >= k) b = c;
+		else a = c;
+	return b;
+}
+
+int main() {
+	scanf("%d%*d%d", &m, &l);
+	while (m--) { scanf("%d", &x);  v.push_back(x); }
+	sort(v.begin(), v.end()); m = v.size() - 1;
+	while (~scanf("%d%d", &x, &y)) 
+    if (v[f(x + y - l)] <= x - y + l)
+      z++;
+	printf("%d", z);
+}
